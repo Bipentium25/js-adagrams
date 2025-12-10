07234b920732a3f60261ef502f80358fffb74bf3
Bipentium25 <bipentium25@gmail.com>
2025-12-09 20:35:03 -0800

    Update src/adagrams.js
    Co-authored-by: Mikelle <119449245+mikellewade@users.noreply.github.com>

--- src/adagrams.js
+++ src/adagrams.js
@@ -16,7 +16,7 @@ export const drawLetters = () => {
 
   const myHand = [];
   for (let i = 0; i < 10; i++){
-    const last = letterPool.length-1;
+    const last = letterPool.length - 1;
     let letterIndex = Math.floor(Math.random()*letterPool.length);
     // eslint-disable-next-line no-self-assign
     letterPool[letterIndex], letterPool[last] = letterPool[last],letterPool[letterIndex];
